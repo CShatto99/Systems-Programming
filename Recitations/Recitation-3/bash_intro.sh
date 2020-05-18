c5ea443fc30d43d2e04635deba4c0b952f010514
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2020-05-17 22:32:21 -0500

    Add files via upload

---
+++ Recitations/Recitation-3/bash_intro.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+clear
+
+echo "Enter a string: "
+read STRING
+echo
+
+for WORD in $STRING
+do
+	echo $WORD
+done
+echo
+
+for FILE in *
+do
+	BYTES=`stat -c %s $FILE`
+	KBYTES=`ls -sh $FILE`
+	echo "$BYTES bytes $KBYTES"
+done
+echo
